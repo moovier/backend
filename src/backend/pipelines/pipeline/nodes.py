df4d737da5b33e27c79369516b7df824861785c6
Lizaveta Babior <lizaveta.babior@olx.pl>
2023-11-20 11:42:44 +0100

    fix: movies title format

--- src/backend/pipelines/pipeline/nodes.py
+++ src/backend/pipelines/pipeline/nodes.py
@@ -5,12 +5,28 @@ from kedro.pipeline import node
 from .model import train_recommender
 
 
+def drop_year_and_translation(movies):
+    def clean_title(title):
+        # Remove any data in parentheses (year/translation)
+        title_clear = title.split(" (")[0]
+        # Split the title on commas
+        parts = title_clear.split(", ")
+        # Swap parts if there's a comma
+        if len(parts) > 1:
+            return " ".join(parts[::-1])
+        return title_clear
+
+    movies["title"] = movies["title"].apply(clean_title)
+
+    return movies
+
+
 def drop_genres(movies):
-    return movies.drop(columns='genres')
+    return movies.drop(columns="genres")
 
 
 def drop_timestamp(ratings):
-    return ratings.drop(columns='timestamp')
+    return ratings.drop(columns="timestamp")
 
 
 def index_ratings(ratings):
@@ -23,9 +39,9 @@ def index_ratings(ratings):
     _, users_to_indices = create_index_maps(ratings.userId)
     indices_to_movies, movies_to_indices = create_index_maps(ratings.movieId)
 
-    ratings['user'] = ratings.userId.map(users_to_indices)
-    ratings['movie'] = ratings.movieId.map(movies_to_indices)
-    ratings['rating'] = ratings.rating.values.astype(np.float32)
+    ratings["user"] = ratings.userId.map(users_to_indices)
+    ratings["movie"] = ratings.movieId.map(movies_to_indices)
+    ratings["rating"] = ratings.rating.values.astype(np.float32)
 
     return ratings, movies_to_indices, indices_to_movies, users_to_indices
 
@@ -33,36 +49,48 @@ def index_ratings(ratings):
 def normalize_ratings(ratings):
     min_rating, max_rating = min(ratings.rating), max(ratings.rating)
 
-    def normalizer(x): return (x - min_rating) / (max_rating - min_rating)
+    def normalizer(x):
+        return (x - min_rating) / (max_rating - min_rating)
 
-    ratings['rating'] = ratings['rating'].apply(normalizer).values
+    ratings["rating"] = ratings["rating"].apply(normalizer).values
     ratings.sample(frac=1, random_state=42)
     return ratings
 
 
 def train_model(ratings, training_split_ratio, embedding_size, learning_rate):
     return train_recommender(
-        x=ratings[['user', 'movie']].values,
-        y=ratings['rating'],
-        num_users=ratings['user'].nunique(),
-        num_movies=ratings['movie'].nunique(),
+        x=ratings[["user", "movie"]].values,
+        y=ratings["rating"],
+        num_users=ratings["user"].nunique(),
+        num_movies=ratings["movie"].nunique(),
         embedding_size=embedding_size,
         learning_rate=learning_rate,
-        train_split=int(training_split_ratio * ratings.shape[0])
+        train_split=int(training_split_ratio * ratings.shape[0]),
     )
 
 
-def recommend_movies(model, movies, ratings, movies_to_indices, indices_to_movies, users_to_indices, top_k, num_users):
+def recommend_movies(
+    model,
+    movies,
+    ratings,
+    movies_to_indices,
+    indices_to_movies,
+    users_to_indices,
+    top_k,
+    num_users,
+):
     recommended_movies_for_all_users = []
 
-    for user_id in ratings['userId'][:num_users]:
-        movies_watched = ratings[ratings['userId'] == user_id]
+    for user_id in ratings["userId"][:num_users]:
+        movies_watched = ratings[ratings["userId"] == user_id]
 
-        movies_not_watched = ~movies['movieId'].isin(movies_watched['movieId'].values)
-        movies_not_watched = movies[movies_not_watched]['movieId']
+        movies_not_watched = ~movies["movieId"].isin(movies_watched["movieId"].values)
+        movies_not_watched = movies[movies_not_watched]["movieId"]
         movies_not_watched = set(movies_not_watched) & set(movies_to_indices.keys())
         movies_not_watched = list(movies_not_watched)
-        movies_not_watched = [[movies_to_indices.get(movie)] for movie in movies_not_watched]
+        movies_not_watched = [
+            [movies_to_indices.get(movie)] for movie in movies_not_watched
+        ]
 
         user_encoder = users_to_indices.get(user_id)
 
@@ -74,72 +102,82 @@ def recommend_movies(model, movies, ratings, movies_to_indices, indices_to_movie
         ratings_sorted_indices = ratings_pred.argsort()[-top_k:][::-1]
 
         recommended_movies = [
-            indices_to_movies.get(movies_not_watched[rating][0]) for rating in ratings_sorted_indices
+            indices_to_movies.get(movies_not_watched[rating][0])
+            for rating in ratings_sorted_indices
         ]
-        recommended_movies = movies[movies['movieId'].isin(recommended_movies)]['title']
+        recommended_movies = movies[movies["movieId"].isin(recommended_movies)]["title"]
 
-        recommended_movies_for_all_users.append({'userId': user_id, 'recommendations': '|'.join(recommended_movies)})
+        recommended_movies_for_all_users.append(
+            {"userId": user_id, "recommendations": "|".join(recommended_movies)}
+        )
 
     return pd.DataFrame(recommended_movies_for_all_users)
 
 
+drop_year_and_translation_node = node(
+    func=drop_year_and_translation,
+    inputs="movies",
+    outputs="cleaned_title",
+    name=drop_year_and_translation.__name__,
+)
+
 drop_genres_node = node(
     func=drop_genres,
-    inputs='movies',
-    outputs='cleaned_movies',
-    name=drop_genres.__name__
+    inputs="cleaned_title",
+    outputs="cleaned_movies",
+    name=drop_genres.__name__,
 )
 
 drop_timestamp_node = node(
     func=drop_timestamp,
-    inputs='ratings',
-    outputs='cleaned_ratings',
-    name=drop_timestamp.__name__
+    inputs="ratings",
+    outputs="cleaned_ratings",
+    name=drop_timestamp.__name__,
 )
 
 index_ratings_node = node(
     func=index_ratings,
-    inputs='cleaned_ratings',
+    inputs="cleaned_ratings",
     outputs=[
-        'indexed_ratings',
-        'movies_to_indices',
-        'indices_to_movies',
-        'users_to_indices'
+        "indexed_ratings",
+        "movies_to_indices",
+        "indices_to_movies",
+        "users_to_indices",
     ],
-    name=index_ratings.__name__
+    name=index_ratings.__name__,
 )
 
 normalize_ratings_node = node(
     func=normalize_ratings,
-    inputs='indexed_ratings',
-    outputs='normalized_ratings',
-    name=normalize_ratings.__name__
+    inputs="indexed_ratings",
+    outputs="normalized_ratings",
+    name=normalize_ratings.__name__,
 )
 
 train_model_node = node(
     func=train_model,
     inputs=[
-        'normalized_ratings',
-        'params:training_split_ratio',
-        'params:embedding_size',
-        'params:learning_rate'
+        "normalized_ratings",
+        "params:training_split_ratio",
+        "params:embedding_size",
+        "params:learning_rate",
     ],
-    outputs='trained_model',
-    name=train_model.__name__
+    outputs="trained_model",
+    name=train_model.__name__,
 )
 
 recommend_movies_node = node(
     func=recommend_movies,
     inputs=[
-        'trained_model',
-        'cleaned_movies',
-        'normalized_ratings',
-        'movies_to_indices',
-        'indices_to_movies',
-        'users_to_indices',
-        'params:top_k',
-        'params:num_users'
+        "trained_model",
+        "cleaned_movies",
+        "normalized_ratings",
+        "movies_to_indices",
+        "indices_to_movies",
+        "users_to_indices",
+        "params:top_k",
+        "params:num_users",
     ],
-    outputs='recommended_movies',
-    name=recommend_movies.__name__
+    outputs="recommended_movies",
+    name=recommend_movies.__name__,
 )
--- src/backend/pipelines/pipeline/pipeline.py
+++ src/backend/pipelines/pipeline/pipeline.py
@@ -1,13 +1,22 @@
 from kedro.pipeline import Pipeline, pipeline
-from .nodes import drop_genres_node, drop_timestamp_node, index_ratings_node, normalize_ratings_node, train_model_node, recommend_movies_node
-
-actions = [
+from .nodes import (
+    drop_year_and_translation_node,
     drop_genres_node,
     drop_timestamp_node,
     index_ratings_node,
     normalize_ratings_node,
     train_model_node,
-    recommend_movies_node
+    recommend_movies_node,
+)
+
+actions = [
+    drop_year_and_translation_node,
+    drop_genres_node,
+    drop_timestamp_node,
+    index_ratings_node,
+    normalize_ratings_node,
+    train_model_node,
+    recommend_movies_node,
 ]
 
 
